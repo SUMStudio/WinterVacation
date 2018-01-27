a60e2713a3593e803b2af27dc085022ba2881741
张俊华 <937734121@qq.com>
2018-01-27 21:20:04 +0800

    DAY 7

--- workspace/nand2tetris/SincereXIA/Day6/04/fill/Fill.asm
+++ workspace/nand2tetris/SincereXIA/Day6/04/fill/Fill.asm
@@ -12,3 +12,39 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+@SCREEN
+D=A
+@R0
+M=D //初始化该处理的像素位
+(FOREVER)
+@KBD
+D=M //从键盘读取输入
+@FILL
+D;JNE //若有输入，跳过清零，点亮像素
+@SCREEN
+D=A
+@R0
+D=M-D
+@FOREVER
+D;JLT //清零出界则取消清零
+@R0
+A=M
+M=0 //对应像素点清屏
+@R0
+M=M-1
+@FOREVER
+0;JMP //单次清屏结束，跳转检测输入
+(FILL)
+@24576
+D=A
+@R0
+D=D-M
+@FOREVER
+D;JLE //点亮出界则取消点亮
+@R0
+A=M
+M=-1 //对应像素点亮
+@R0
+M=M+1
+@FOREVER
+0;JMP
